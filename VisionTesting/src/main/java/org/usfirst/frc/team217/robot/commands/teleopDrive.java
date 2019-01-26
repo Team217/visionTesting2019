e4567a57141cf1fef329d2d353b7033f8b425e01
Benjamin Hall <hallb1016@gmail.com>
2019-01-26 17:01:18 -0500

    Added Code to Follow Tape

--- VisionTesting/src/main/java/org/usfirst/frc/team217/robot/commands/teleopDrive.java
+++ VisionTesting/src/main/java/org/usfirst/frc/team217/robot/commands/teleopDrive.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class teleopDrive extends Command {
 	PID turnPID1 = RobotMap.turnPID;
 	PID angleCorrectPID = new PID(0.01, 0.0, 0.0, 100);
+	PID followPID = new PID(0, 0.06, 0, 100);
 
 	public boolean drivePID_OnTarget = false;
 	public boolean turnPID_OnTarget = false;
@@ -35,24 +36,29 @@ public class teleopDrive extends Command {
 			double x = Robot.getXVis();
 			double area = Robot.getAreaVis();
 			
-			double kP = Range.inRange(.03 / Math.sqrt(area) - .01, 0.015, 0.02);
+			double kP = Range.inRange(.03 / Math.sqrt(area) - .01, 0.015, 0.025);
 			
-			turnPID1.setPID(kP, 0.003, 0.005);
+			turnPID1.setPID(kP, 0.002, 0);
 			turnPID1.setMinMax(-0.2, 0.2);
 			
-			double visSpeed;
+			double visTurn;
 
 			if(Range.isWithinRange(x, -0.5, 0.5)) {
-				visSpeed = 0.0;
+				visTurn = 0.0;
 				turnPID_OnTarget = true;
 			}
 			else {
-				visSpeed = turnPID1.getOutput(0, x);
+				visTurn = turnPID1.getOutput(0, x);
 				
 				turnPID_OnTarget = false;
 			}
 
-    		turn = visSpeed;
+			turn = visTurn;
+			
+			followPID.setP(0.64);
+			followPID.setMinMax(-0.2, 0.2);
+			double visSpeed = -followPID.getOutput(Math.sqrt(area), 0.98);
+			speed = (area != 0) ? Range.inRange(visSpeed, -0.6, 0.6) : 0;
     	}
     	
     	if(Robot.m_oi.leftBumperDriver.get()) {
